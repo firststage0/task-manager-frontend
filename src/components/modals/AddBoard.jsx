a7fd76855f43613ddc13d1a88d9ab5c6e2e1d9da
firststage0 <red_tk@mail.ru>
2025-01-06 01:14:46 +0300

    Сделано создание доски и привязан метод с бэка

--- src/components/modals/AddBoard.jsx
+++ src/components/modals/AddBoard.jsx
@@ -1,16 +1,50 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import Image from "next/image";
+import { useDetectClickOutside } from "react-detect-click-outside";
+import { domain } from "@/utils/domain";
 
 export default function AddBoard() {
     const [showModal, setShowModal] = useState(false);
+    const [boardName, setBoardName] = useState("");
+    const ref = useDetectClickOutside({
+        onTriggered: () => closeModal(),
+    });
+    let boardRes;
+    const closeModal = () => {
+        setBoardName("");
+        setShowModal(false);
+    };
+    const createBoard = async () => {
+        const boardReq = await fetch(`${domain}/api/boards`, {
+            method: "POST",
+            body: JSON.stringify({
+                name: boardName,
+                authorId: "cm5jw5xo80000vevkdppbehr7",
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (boardReq.ok) {
+            boardRes = await boardReq.json();
+        }
+
+        console.log(boardRes);
+        closeModal();
+    };
+
     const modalContent = (
         <div className="fixed top-0 left-0 w-full h-dvh flex items-center justify-center">
-            <div className=" w-[300px] border-2 border-gray-700 shadow-2xl bg-white rounded-2xl px-4 py-2 flex flex-col items-end gap-2">
+            <div
+                ref={ref}
+                className=" w-[300px] border-2 border-gray-700 shadow-2xl bg-white rounded-2xl px-4 py-2 flex flex-col items-end gap-2"
+            >
                 <div className="flex items-center justify-between w-full">
                     <p className="text-1xl font-bold">Create new board</p>
-                    <button onClick={() => setShowModal(false)}>
+                    <button onClick={() => closeModal()}>
                         <Image
                             src="/close.png"
                             alt="close"
@@ -23,8 +57,12 @@ export default function AddBoard() {
                     type="text"
                     placeholder="Board name"
                     className="outline-none px-4 py-2 w-full font-medium rounded-xl border-2 border-gray-400"
+                    onChange={(e) => setBoardName(e.target.value)}
                 />
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-xl w-full">
+                <button
+                    className="bg-blue-600 text-white px-4 py-2 rounded-xl w-full"
+                    onClick={createBoard}
+                >
                     Create
                 </button>
             </div>
